7c610fc5a610a8da912cee7c1eb27997bf6f515d
Emmankoko <emmankoko519@gmail.com>
2023-11-25 01:09:17 +0000

    let's see what to do

--- source/stdcpp/list.d
+++ source/stdcpp/list.d
@@ -312,7 +312,144 @@ extern(C++, class) struct list(Type, Allocator)
 
 		}
 		}
-		else version(CppRuntime_Microsft)
+		else version(CppRuntime_Clang)
 		{
+			this(def)
+			{
+				allocator!Type alloc_instance = allocator!(Type).init;
+				this(alloc_instance);
+			}
+
+
+			 //allocator ctor
+			this(ref const allocator!Type);
+
+			//copy ctor
+			this(ref const list!Type __x);
+
+			 //list(n,value) ctor
+			this(size_type __n, ref const value_type value, ref const allocator!Type);
+
+			extern(D) this(size_type n, const value_type element)
+			{
+				allocator!Type alloc_instance = allocator!(Type).init;
+				this(n, element, alloc_instance);
+			}
+
+			this(ref const list!Type other, ref const allocator!Type);
+
+			//list(n) ctor
+			this(size_type __n, ref const allocator!Type);
+
+			extern(D) this(size_type n)
+			{
+				allocator!Type alloc_instance = allocator!(Type).init;
+				this(n, alloc_instance);
+			}
+
+//			~this();
+
+			extern(D) void assign(size_type n, const value_type item)
+			{
+				this.assign(n, item);
+			}
+
+			extern(D) void push_back(const Type item)
+			{
+				this.push_back(item);
+			}
+
+			extern(D) void push_front(const Type item)
+			{
+				this.push_front(item);
+			}
+
+			extern(D) void resize(size_type n, const value_type item)
+			{
+				this.resize(n, item);
+			}
+
+			extern(D) void remove(const value_type item)
+			{
+				this.remove(item);
+			}
+
+/*			extern(D) size_type remove(const value_type item)
+			{
+				return this.remove(item);
+			}
+*/
+			ref list opAssign(ref const list!Type other);
+
+			void assign(size_type count, ref const value_type value);
+
+			//const nothrow since C++11
+			allocator_type get_allocator() const nothrow;
+
+			ref value_type front();
+
+//			const(value_type) ref front() const;
+
+			ref value_type back();
+
+//			ref const(value_type) back() const;
+
+			pointer begin() nothrow;
+
+			pointer end() nothrow;
+
+			//const nothrow since c++11
+			bool empty() const nothrow;
+
+			size_type size() const nothrow;
+
+			void clear() nothrow;
+
+			//insert halted for now
+
+			void push_back(ref const Type val);
+
+			void pop_back();
+
+			void push_front(ref const value_type val);
+
+			void pop_front();
+
+			void resize(size_type count);
+
+			void resize(size_type count, ref const value_type val);
+
+			void swap(ref const list!Type other) nothrow;
+
+			void merge( ref const list!Type other);
+
+			void merge(U)(ref const list!Type other, U comp);
+
+			void remove(const ref value_type val);
+
+//			size_type remove(const ref value_type val);
+
+			void reverse() nothrow;
+
+			void sort();
+
+			void sort(U)(U comp);
+
+			void unique();
+
+			void unique(U)(U p);
+
+			size_type unique();
+
+			size_type unique(U)(U p);
+
+		private:
+			struct node
+			{
+				node* prev;
+				node* next;
+			}
+			node A;
+			size_type _M_size;
 		}
 }
