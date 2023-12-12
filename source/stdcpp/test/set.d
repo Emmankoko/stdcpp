5114045d9f1d87282472b1a5a9137a842110615a
Emmankoko <emmankoko519@gmail.com>
2023-12-12 08:20:35 +0000

    adding test for erase
    this test for erase is a further step test for size and empty which
    sends size back to 0 and empty to 1

--- source/stdcpp/test/set.d
+++ source/stdcpp/test/set.d
@@ -11,8 +11,11 @@ import stdcpp.set;
 unittest
 {
 	less!int a;
-    auto p = set!int(a);
-    p.insert(5);
-    assert(p.size == 1);
-    assert(p.empty == 0);
+	auto p = set!int(a);
+	p.insert(5);
+	assert(p.size == 1);
+	assert(p.empty == 0);
+	p.erase(5);
+	assert(p.size == 0);
+	assert(p.empty == 1);
 }
