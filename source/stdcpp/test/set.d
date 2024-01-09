acba29330e907f971677e17edb7b2fd2c0ad9789
Emmankoko <emmankoko519@gmail.com>
2024-01-09 18:38:50 +0000

    current test level working

--- source/stdcpp/test/set.d
+++ source/stdcpp/test/set.d
@@ -20,24 +20,4 @@ unittest
 	p.clear;
 	assert(p.size == 0);
 	assert(p.empty == 1);
-/*	p.erase(5);
-	p.insert(6);
-	p.clear;
-	assert(p.size == 0);
-	assert(p.empty == 1);
-	set!int q = a;
-	q.swap(p);
-	q.insert(4);
-	q.insert(4);
-	q.insert(4);
-	assert(q.size == 1);
-	assert(q.count(4) == 1);//count for set only results in 0 for not present or 1 for present
-	assert(q.count(5) == 0);
-	assert(q.sizeof == 48);
-	assert(q.contains(4) == 1); // q contains 4 evaluates to true
-	auto iter = q.find(4);
-	set!int w = q; //copy constructor
-//	assert(*iter == 4);
-	
-*/
 }
