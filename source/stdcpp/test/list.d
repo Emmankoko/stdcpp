1e51fcbea8de781917d2500fef89780185445571
Emmanuel Nyarko <joe@Emmanuels-MacBook-Pro.local>
2024-02-07 12:30:57 +0000

    SEE

--- source/stdcpp/test/list.d
+++ source/stdcpp/test/list.d
@@ -7,7 +7,8 @@
 module stdcpp.test.list;
 
 import stdcpp.list;
-
+version (CppRuntime_Gcc)
+{
 unittest
 {
     auto p = list!int(5);
@@ -37,4 +38,42 @@ unittest
     assert(cp_obj.front == 45);
     assert(cp_obj.back == 56);
     cp_obj.clear();
+}
+}
+else version (CppRuntime_Clang)
+{
+unittest
+{
+	auto p = list!int(5,5);
+	p.push_back(5);
+	p.push_front(7);
+	p.assign(5,5);
+	p.pop_front();
+	p.resize(3);
+	p.clear();
+	assert(p.empty() == 1);
+
+	list!int cp_obj = p; //opAssign
+	cp_obj.push_back(45);
+	cp_obj.push_back(56);
+	auto newctor = list!int(4);
+	newctor.push_back(6);
+	assert(newctor.front == 0);// test for front
+	assert(newctor.back == 6);
+	assert(newctor.empty == 0);
+	assert(newctor.size() == 5); //slow testing for size
+	auto cpctor = list!int(newctor); // copy constructor
+	assert(cpctor.sizeof == 24); //checking for 24 byte size for list on Mac
+	cp_obj.remove(45);
+	cp_obj.clear();
+	assert(cp_obj.empty == 1);
+
+	auto new_list = list!int(7);
+	assert(new_list.size == 7);
+	assert(new_list.front == 0);
+	assert(new_list.empty == 0);
+	assert(new_list.back == 0);
+	new_list.clear();
+	assert(new_list.empty == 1);
+}
 }
