1b717eb5385c31fedec7db5948f45cc8c7652cd2
Emmanuel Nyarko <joe@Emmanuels-MacBook-Pro.local>
2024-05-17 14:58:06 +0000

    tests suits for map

---
+++ source/stdcpp/test/map.d
@@ -0,0 +1,16 @@
+/*******************************************************************************
+
+    Tests for `std::map`
+
+*******************************************************************************/
+
+module stdcpp.test.map;
+
+import stdcpp.map;
+
+unittest
+{
+    auto mymap = Map!(int, char).make();
+    mymap.opIndexAssign(4, 'a');
+}
+
