10959d84b79ca398755376e8df415b749e61c8b9
Emmanuel Nyarko <joe@Emmanuels-MacBook-Pro.local>
2024-06-01 09:10:41 +0000

    add documentation generator to interfaces

--- source/stdcpp/map.d
+++ source/stdcpp/map.d
@@ -13,46 +13,47 @@ module stdcpp.map;
  extern(C++, class):
  struct Map(Key, Value)
  {
+    ///
     extern(D) void opIndexAssign (const Key key, Value value)
     {
         this.insertOrAssign(key, value);
     }
-
+    ///
     static Map* make();
-
+    ///
     private void insertOrAssign(const ref Key, const ref Value);
-
+    ///
     size_t size();
-
+    ///
     ref Value opIndex( ref const Key key);
-
+    ///
     ref Value opIndex(const Key key)
     {
         return this.opIndex(key); // handle rvalue-ref
     }
-
+    ///
     ref Value at( ref const Key key);
-
+    ///
     extern(D) ref Value at(const Key key)
     {
         return this.at(key);
     }
-
+    ///
     bool empty() const nothrow;
-
+    ///
     size_t max_size() const;
-
+    ///
     void clear() nothrow;
-
+    ///
     size_t count(ref const Key key) const;
-
+    ///
     extern(D) size_t count(const Key key) const
     {
         return this.count(key);
     }
-
+    ///
     bool contains( ref const Key key) const;
-
+    ///
     extern(D) bool contains(const Key key) const
     {
         return this.contains(key);
